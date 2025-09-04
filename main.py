4c00f25b770244069b5b28017dc6cbaeadd42904
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-09-04 09:55:37 -0600

    etiqueta y entrada

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ app = tk.Tk()
 app.title("cAlCuLaDoRa")
 app.geometry("300x300")
 app.minsize(300, 300)
+
+etiqueta = tk.Label(app, text="Calculadora")
+etiqueta.pack(pady=5)
+
+entrada = tk.Entry(app)
+entrada.pack(pady=5)
+
